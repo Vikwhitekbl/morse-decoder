6265524d16f73fcb6a1c7d8a5e440e14d91b7474
Viktor Belov <vbelov@vansofts.net>
2023-09-28 21:38:03 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,27 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****':  ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    const letter = expr.match(/.{1,10}/g);
+    const symb = letter.map(letter => letter.match(/.{1,2}/g));
+    const objKey = {'00': '', '10': '.', '11': '-', '**': '*'};
+
+    const masKey = symb.map(subArray => [subArray.map(key => objKey[key]).join('')]);
+
+    for (let i = 0; i < masKey.length; i += 1) {
+        for (let j = 0; j < masKey[i].length; j += 1) {
+            if (MORSE_TABLE.hasOwnProperty(masKey[i][j])) {
+                masKey[i][j] = MORSE_TABLE[masKey[i][j]];
+            }
+        }
+    }
+    const subResult = masKey.flat();
+    const result = subResult.join('');
+    return result
 }
 
 module.exports = {
